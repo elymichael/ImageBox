93fa18a5fa975db2a123090bb4fb5b92cc54263a
Ely Michael Núñez De la Rosa <elymichael@gmail.com>
2020-06-28 22:18:19 -0400

    Adding phone status.

--- ImageBox/ImageBox/Pages/SettingsPage.xaml.cs
+++ ImageBox/ImageBox/Pages/SettingsPage.xaml.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace ImageBox.Pages
+﻿namespace ImageBox.Pages
 {
+    using System.Windows.Input;
+    using Xamarin.Essentials;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
@@ -17,8 +13,26 @@ namespace ImageBox.Pages
             InitializeComponent();
 
             BindingContext = this;
+
+            loadConfiguration();
         }
 
+        private void loadConfiguration()
+        {
+            var geocodeAddress =
+                        $"AppName:     {AppInfo.Name}\n" +
+                        $"AppBuild:     {AppInfo.BuildString}\n" +
+                        $"Device:     {DeviceInfo.Model}\n" +
+                        $"Manufacturer:     {DeviceInfo.Manufacturer}\n" +
+                        $"DeviceName:     {DeviceInfo.Name}\n" +
+                        $"Version:        {DeviceInfo.VersionString}\n" +
+                        $"Platform:      {DeviceInfo.Platform}\n" +
+                        $"Idiom:    {DeviceInfo.Idiom}\n" +
+                        $"DeviceType:     {DeviceInfo.DeviceType}\n"+
+                        $"Package:     {AppInfo.PackageName}\n";
+
+            lblApplicationInfo.Text = (geocodeAddress);
+        }
 
         public ICommand ClosePage => new Command(OnDismissButtonClicked);
 
